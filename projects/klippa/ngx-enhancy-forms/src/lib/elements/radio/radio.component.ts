fdba78a4a607c2d253f4be486a7f39554af914e3
Andreea <bociu.andreea@gmail.com>
2023-05-04 15:54:04 +0200

    fixed not compiling error

--- projects/klippa/ngx-enhancy-forms/src/lib/elements/radio/radio.component.ts
+++ projects/klippa/ngx-enhancy-forms/src/lib/elements/radio/radio.component.ts
@@ -1,12 +1,12 @@
-import {Component, Input} from '@angular/core';
-import {NG_VALUE_ACCESSOR} from '@angular/forms';
-import {ValueAccessorBase} from '@klippa/ngx-enhancy-forms';
+import { Component, Input } from '@angular/core';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
+import { ValueAccessorBase } from '../value-accessor-base/value-accessor-base.component';
 
 @Component({
 	selector: 'klp-form-radio',
 	templateUrl: './radio.component.html',
 	styleUrls: ['./radio.component.scss'],
-	providers: [{ provide: NG_VALUE_ACCESSOR, useExisting: RadioComponent, multi: false }],
+	providers: [{ provide: NG_VALUE_ACCESSOR, useExisting: RadioComponent, multi: true }],
 })
 export class RadioComponent extends ValueAccessorBase<boolean> {
 	@Input() items: Array<string> = [];
