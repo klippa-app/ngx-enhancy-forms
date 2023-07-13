8128a412a4260d270a38bae2c4e8cc755a812dfd
Wouter Willems <wouter@klippa.com>
2023-07-13 16:34:21 +0200

    support separating lines

--- projects/klippa/ngx-enhancy-forms/src/lib/elements/select/select.component.ts
+++ projects/klippa/ngx-enhancy-forms/src/lib/elements/select/select.component.ts
@@ -48,6 +48,7 @@ export class SelectComponent extends ValueAccessorBase<string | string[]> implem
 	@Input() multipleDisplayedAsAmount = false;
 	@Input() clearable = true;
 	@Input() truncateOptions = true;
+	@Input() withSeparatingLine = false;
 	@Input() public dropdownPosition: 'auto' | 'bottom' | 'top' | 'left' | 'right' = null;
 	@Input() public customSearchFn: (term: string, item: { id: string; name: string; description: string }) => boolean;
 	@Input() public footerElement: TemplateRef<any>;
