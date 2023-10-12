9923dbcc988fc01129142903d8cca7bdfd0d654b
Wouter Willems <wouter@klippa.com>
2023-10-12 13:22:13 +0200

    improve styling of tooltip errors

--- projects/klippa/ngx-enhancy-forms/src/lib/withTooltip.component.ts
+++ projects/klippa/ngx-enhancy-forms/src/lib/withTooltip.component.ts
@@ -1,11 +1,14 @@
 import {Directive, ElementRef, Input} from "@angular/core";
 
+const triangleSize = '12px';
+
 @Directive({
 	selector: '[klpWithTooltip]'
 })
 export class WithTooltipDirective {
 	private div: HTMLElement;
 	private triangle: HTMLElement;
+	private triangleWhite: HTMLElement;
 	@Input() klpWithTooltip = true;
 	constructor(el: ElementRef) {
 		el.nativeElement.addEventListener('mouseenter', () => {
@@ -18,7 +21,7 @@ export class WithTooltipDirective {
 
 			this.div = document.createElement('div');
 			this.div.style.zIndex = '2';
-			this.div.style.color = '#515365';
+			this.div.style.color = '#ff8000';
 			this.div.style.position = 'fixed';
 			this.div.style.left = `${el.nativeElement.getBoundingClientRect().x}px`;
 			this.div.style.top = `${el.nativeElement.getBoundingClientRect().y}px`;
@@ -37,15 +40,28 @@ export class WithTooltipDirective {
 			this.triangle = document.createElement('div');
 			this.triangle.style.zIndex = '1';
 			this.triangle.style.position = 'fixed';
-			this.triangle.style.left = `calc(${el.nativeElement.getBoundingClientRect().x + el.nativeElement.getBoundingClientRect().width}px - 3rem)`;
+			this.triangle.style.left = `calc(${el.nativeElement.getBoundingClientRect().x + el.nativeElement.getBoundingClientRect().width}px - 2rem)`;
 			this.triangle.style.top = `${el.nativeElement.getBoundingClientRect().y}px`;
 			this.triangle.style.transform = `translate(-50%, calc(-100% + 0.1rem))`;
 			this.triangle.style.width = '0';
 			this.triangle.style.height = '0';
-			this.triangle.style.borderLeft = '0.8rem solid transparent';
-			this.triangle.style.borderRight = '0.8rem solid transparent';
-			this.triangle.style.borderTop = '0.8rem solid rgba(255, 128, 0, 0.1254901961)';
+			this.triangle.style.borderLeft = `${triangleSize} solid transparent`;
+			this.triangle.style.borderRight = `${triangleSize} solid transparent`;
+			this.triangle.style.borderTop = `${triangleSize} solid rgba(255, 128, 0, 0.1254901961)`;
 			el.nativeElement.prepend(this.triangle);
+
+			this.triangleWhite = document.createElement('div');
+			this.triangleWhite.style.zIndex = '3';
+			this.triangleWhite.style.position = 'fixed';
+			this.triangleWhite.style.left = `calc(${el.nativeElement.getBoundingClientRect().x + el.nativeElement.getBoundingClientRect().width}px - 2rem)`;
+			this.triangleWhite.style.top = `${el.nativeElement.getBoundingClientRect().y}px`;
+			this.triangleWhite.style.transform = `translate(-50%, calc(-100% + 0.1rem - 2px))`;
+			this.triangleWhite.style.width = '0';
+			this.triangleWhite.style.height = '0';
+			this.triangleWhite.style.borderLeft = `${triangleSize} solid transparent`;
+			this.triangleWhite.style.borderRight = `${triangleSize} solid transparent`;
+			this.triangleWhite.style.borderTop = `${triangleSize} solid white`;
+			el.nativeElement.prepend(this.triangleWhite);
 		});
 		el.nativeElement.addEventListener('mouseout', () => {
 			try {
@@ -53,6 +69,9 @@ export class WithTooltipDirective {
 			} catch (ex) {}
 			try {
 				el.nativeElement.removeChild(this.triangle);
+			} catch (ex) {}
+			try {
+				el.nativeElement.removeChild(this.triangleWhite);
 			} catch (ex) {}
 		});
 	}
