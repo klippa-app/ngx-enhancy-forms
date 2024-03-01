4c88b66eadaaf50c0d73b47602266a84d903f31d
joaogoncalves <joao@klippa.com>
2024-03-01 14:20:10 +0100

    add disabled status to file input

--- projects/klippa/ngx-enhancy-forms/src/lib/elements/file-input/file-input.component.ts
+++ projects/klippa/ngx-enhancy-forms/src/lib/elements/file-input/file-input.component.ts
@@ -16,6 +16,7 @@ export class FileInputComponent extends MultipleValueAccessorBase<File> {
 	@Input() onlyShowUploadButton = false;
 	@Input() useFullParentSize = false;
 	@Input() buttonText: string = 'Upload a file';
+	@Input() disabled: boolean = false;
 	@ViewChild('nativeInputRef') nativeInputRef: ElementRef<HTMLInputElement>;
 
 	public onChange(files: FileList): void {
