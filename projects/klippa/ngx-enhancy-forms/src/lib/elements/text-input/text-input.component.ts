e9f56fc160c956b06e352625b08a7dba586a2a32
Wouter Willems <wouter@klippa.com>
2024-07-19 19:50:56 +0200

    actually spell 'peeking' correctly

--- projects/klippa/ngx-enhancy-forms/src/lib/elements/text-input/text-input.component.ts
+++ projects/klippa/ngx-enhancy-forms/src/lib/elements/text-input/text-input.component.ts
@@ -9,7 +9,7 @@ import {ValueAccessorBase} from '../value-accessor-base/value-accessor-base.comp
 	providers: [{provide: NG_VALUE_ACCESSOR, useExisting: TextInputComponent, multi: true}],
 })
 export class TextInputComponent extends ValueAccessorBase<string> {
-	private isPeakingPassword = false;
+	private isPeekingPassword = false;
 
 	@Input() placeholder: string;
 	@Input() type: 'text' | 'password' = 'text';
@@ -17,17 +17,17 @@ export class TextInputComponent extends ValueAccessorBase<string> {
 	@Input() icon: 'search';
 	@Input() hasBorderLeft = true;
 	@Input() hasBorderRight = true;
-	@Input() passwordPeakIcon: TemplateRef<any>;
+	@Input() passwordPeekIcon: TemplateRef<any>;
 	@Output() onBlur = new EventEmitter<void>();
 
 	public togglePeakPassword(): void {
-		this.isPeakingPassword = !this.isPeakingPassword;
+		this.isPeekingPassword = !this.isPeekingPassword;
 	}
 
 	public getType(): 'text' | 'password' {
 		if (this.type === 'text') {
 			return 'text';
 		}
-		return this.isPeakingPassword ? 'text' : 'password';
+		return this.isPeekingPassword ? 'text' : 'password';
 	}
 }
