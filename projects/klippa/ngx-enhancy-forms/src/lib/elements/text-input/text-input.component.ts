98adaa36b50463d8c8344612ee9b5b1ec30bf610
Wouter Willems <wouter@klippa.com>
2024-07-19 19:48:45 +0200

    support passwork peaking

--- projects/klippa/ngx-enhancy-forms/src/lib/elements/text-input/text-input.component.ts
+++ projects/klippa/ngx-enhancy-forms/src/lib/elements/text-input/text-input.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, Output, TemplateRef} from '@angular/core';
 import {NG_VALUE_ACCESSOR} from '@angular/forms';
 import {ValueAccessorBase} from '../value-accessor-base/value-accessor-base.component';
 
@@ -9,11 +9,25 @@ import {ValueAccessorBase} from '../value-accessor-base/value-accessor-base.comp
 	providers: [{provide: NG_VALUE_ACCESSOR, useExisting: TextInputComponent, multi: true}],
 })
 export class TextInputComponent extends ValueAccessorBase<string> {
+	private isPeakingPassword = false;
+
 	@Input() placeholder: string;
 	@Input() type: 'text' | 'password' = 'text';
 	@Input() clearable = false;
 	@Input() icon: 'search';
 	@Input() hasBorderLeft = true;
 	@Input() hasBorderRight = true;
+	@Input() passwordPeakIcon: TemplateRef<any>;
 	@Output() onBlur = new EventEmitter<void>();
+
+	public togglePeakPassword(): void {
+		this.isPeakingPassword = !this.isPeakingPassword;
+	}
+
+	public getType(): 'text' | 'password' {
+		if (this.type === 'text') {
+			return 'text';
+		}
+		return this.isPeakingPassword ? 'text' : 'password';
+	}
 }
