dea3dde994ab6643a3c3fbbc5206df52deb86c4c
Wouter Willems <wouter@klippa.com>
2024-07-31 14:04:22 +0200

    support submitting only via the submit button and not on enter

--- projects/klippa/ngx-enhancy-forms/src/lib/form/form-submit-button/form-submit-button.component.ts
+++ projects/klippa/ngx-enhancy-forms/src/lib/form/form-submit-button/form-submit-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding, inject, Input } from '@angular/core';
+import {Component, HostBinding, inject, Input, OnInit} from '@angular/core';
 import {FormComponent, invalidFieldsSymbol} from '../form.component';
 import { ButtonVariant } from '../../elements/button/button.component';
 import { DefaultErrorHandler, FormValidationError, KLP_FORM_ERROR_HANDLER } from '../form-validation-error/form-validation-error';
@@ -15,9 +15,10 @@ export type SubmitButtonVariant = Extract<ButtonVariant,
 	templateUrl: './form-submit-button.component.html',
 	styleUrls: ['./form-submit-button.component.scss'],
 })
-export class FormSubmitButtonComponent {
+export class FormSubmitButtonComponent implements OnInit{
 	private parentForm = inject(FormComponent, {optional: true});
 	private handleError = inject(KLP_FORM_ERROR_HANDLER, {optional: true}) ?? DefaultErrorHandler;
+	public buttonType: 'submit' | 'button' = 'submit';
 
 	@Input() public isLoading = false;
 	@Input() @HostBinding('class._fullWidth') public fullWidth = false;
@@ -26,6 +27,14 @@ export class FormSubmitButtonComponent {
 	@Input() public before: () => Promise<any> = () => Promise.resolve();
 	@Input() public after: () => Promise<any> = () => Promise.resolve();
 	@Input() public disabled: boolean = false;
+
+	ngOnInit(): void {
+		if (this.parentForm.allowSubmitOn === 'buttonOnly') {
+			this.buttonType = 'button';
+		} else {
+			this.buttonType = 'submit';
+		}
+	}
 
 	private setValidationError = (e: FormValidationError) => {
 		this.parentForm.formGroup.get(e.path)?.setErrors({ message: { value: e.message }});
