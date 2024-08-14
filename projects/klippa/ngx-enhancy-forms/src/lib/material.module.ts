fe95f73ca176a0e3219d313d4176c9f2c42eb1b0
Jeroen Bobbeldijk <jeroen@klippa.com>
2024-08-14 11:53:51 +0200

    Migrate material

--- projects/klippa/ngx-enhancy-forms/src/lib/material.module.ts
+++ projects/klippa/ngx-enhancy-forms/src/lib/material.module.ts
@@ -2,10 +2,10 @@
 
 import { NgModule } from '@angular/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatLegacyFormFieldModule as MatFormFieldModule } from '@angular/material/legacy-form-field';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatNativeDateModule } from '@angular/material/core';
-import { MatLegacyInputModule as MatInputModule } from '@angular/material/legacy-input';
-import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
 	imports: [MatDatepickerModule, MatFormFieldModule, MatNativeDateModule, MatInputModule, MatButtonModule],
