e175ce0dfd4a3a2d87e0abc7471f47a83a53342a
Cezar Martinas <cezar@klippa.com>
2026-02-03 14:46:14 +0100

    Cleanup date picker

--- projects/klippa/ngx-enhancy-forms/src/lib/elements/date-time-picker/date-time-picker.component.ts
+++ projects/klippa/ngx-enhancy-forms/src/lib/elements/date-time-picker/date-time-picker.component.ts
@@ -112,6 +112,9 @@ export class DateTimePickerComponent extends MultipleValueAccessorBase<Date | ty
 		this.minutesOfHour = Array.from({length: 60}, (v, k) => k).filter(e => e % 5 === 0);
 	}
 
+	get buttonSize(): string {
+		return this.size === 'small' ? '20px' : '24px';
+	}
 
 
 	ngAfterViewInit(): void {
