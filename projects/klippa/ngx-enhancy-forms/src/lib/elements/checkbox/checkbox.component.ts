138528a2ef97910dadcf7dc7d96cf079970c9be8
Cezar Martinas <cezar@klippa.com>
2026-02-16 15:33:32 +0100

    Remove console log

--- projects/klippa/ngx-enhancy-forms/src/lib/elements/checkbox/checkbox.component.ts
+++ projects/klippa/ngx-enhancy-forms/src/lib/elements/checkbox/checkbox.component.ts
@@ -25,7 +25,6 @@ export class CheckboxComponent extends ValueAccessorBase<boolean> implements OnI
 		const sizes = this.calculateCheckboxSize();
 		this.checkBoxSize = sizes.size;
 		this.captionSpacing = sizes.captionSpacing;
-		console.log(this.checkBoxSize);
 	}
 
 	override ngOnChanges(changes: SimpleChanges): void {
